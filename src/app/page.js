5a25e149fc1486e6444b0d60053142ea61f22922
Hebert <hebertdev82@gmail.com>
2024-02-08 17:26:26 -0300

    timer

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 "use client"
 import Image from 'next/image'
 import Home from './pages/Home'
+import Timer from './components/Countdown'
 
 export default function Index() {
   return (
-    <main className="flex min-h-screen flex-col justify-between">
-      <Home />
+    <main className="flex min-h-screen flex-col justify-between align-center justify-center text-center text-3xl font-bold mt-28">
+      {/* <Home /> */}
+      <div>
+        <h2 className='align-center justify-center text-center text-3xl font-bold mt-28'>Em propagação!</h2>
+        <h4 className='align-center justify-center text-center text-xl'>Volte em 48h</h4>
+        {/* <Timer /> */}
+      </div>
     </main>
   )
 }
