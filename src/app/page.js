ae01d367360753d90360cce1e1210785b7cffbe5
Hebert Richard <hebertdev82@gmail.com>
2024-02-23 12:33:53 -0300

    feat: construction page

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,13 @@ import Timer from './components/Countdown'
 
 export default function Index() {
   return (
-    <main className="flex min-h-screen flex-col justify-between align-center justify-center text-center text-3xl font-bold mt-28">
-      {/* <Home /> */}
-      <div>
-        <h2 className='align-center justify-center text-center text-3xl font-bold mt-28'>Em propagação!</h2>
-        <h4 className='align-center justify-center text-center text-xl'>Volte em 48h</h4>
-        {/* <Timer /> */}
-      </div>
-    </main>
+    <figure id="container">
+      <Image
+      src="/bg.jpg"
+      width={1920}
+      height={1280}
+      / >
+      <figcaption>RNA Seguros</figcaption>
+    </figure>
   )
 }
