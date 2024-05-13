da9940ab631b24780687178137788db2602c33f5
Hebert <hebertdev82@gmail.com>
2024-05-13 12:48:31 -0300

    feat: including about section

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -1,7 +1,15 @@
 export default function About() {
     return (
-        <>
-            <h2>About</h2>
-        </>
+        <div className="md:mt-16 mt-9 md:mb-28 md:mx-44 mx-8">
+            <h2 className="md:text-5xl text-4xl font-bold text-[#2B3E59] text-center">Sobre a R N A Consultoria em Seguros</h2>
+
+            <hr className="w-48 h-1 mx-auto mb-7 bg-gray-100 border-0 rounded md:mt-9 mt-7 md:mb-14 dark:bg-[#ACA4C0]" />
+            
+            <p className="md:text-2xl md:mb-10 mb-12 text-center">Nossa empresa tem como objetivo proporcionar aos clientes total segurança e tranquilidade na contratação de seu seguro, assessorando na identificação das melhores coberturas para atender suas necessidades, bem como informar todos os direitos e deveres que o cliente terá durante a vigência de seu seguro, assegurando assim maior agilidade na liquidação de eventuais sinistros.</p>
+
+            <p className="md:text-2xl md:mb-10 mb-12 text-center">Trabalhando com muito profissionalismo e qualidade, temos uma nova abordagem para a atuação de uma Consultoria de Seguros, oferecendo um padrão de excelência no atendimento aos nossos clientes, ampliando a oferta de serviços que possam dar suporte às suas necessidades, da sua família e também auxiliando na gestão de benefícios e proteção de sua empresa.</p>
+
+            <p className="md:text-2xl text-center">Estar ao lado dos clientes em todo o processo (começo, meio e fim) é o segredo do sucesso nos dias de hoje e é com isso que estamos trabalhando fortemente.</p>
+        </div>
     )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Slider />
-      {/* <About /> */}
+      <About />
     </>
   );
 }
