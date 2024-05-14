703d214c30989e35b8987ca4d09f3a138cff1008
Hebert <hebertdev82@gmail.com>
2024-05-14 16:04:20 -0300

    fix: correct way for the pages

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,9 @@
-import About from "./components/about";
-import About2 from "./components/about2";
-import Citation from "./components/citation";
-import Contact from "./components/contact";
-import Credits from "./components/credits";
-import Footer from "./components/footer";
-import Operation from "./components/operation";
-import Slider from "./components/slider";
+import HomePage from "./pages/home";
 
 export default function Home() {
   return (
     <>
-      <Slider />
-      <About />
-      <About2 />
-      <Operation />
-      <Contact />
-      <Citation />
-      <Footer />
-      <Credits />
+      <HomePage />
     </>
   );
 }
---
+++ app/pages/home.jsx
@@ -0,0 +1,23 @@
+import About from "../components/about";
+import About2 from "../components/about2";
+import Citation from "../components/citation";
+import Contact from "../components/contact";
+import Credits from "../components/credits";
+import Footer from "../components/footer";
+import Operation from "../components/operation";
+import Slider from "../components/slider";
+
+export default function HomePage() {
+  return (
+    <>
+      <Slider />
+      <About />
+      <About2 />
+      <Operation />
+      <Contact />
+      <Citation />
+      <Footer />
+      <Credits />
+    </>
+  );
+}
