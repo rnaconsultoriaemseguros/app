7cfe9bedc36b22eecbecac2fca883f3435a4b737
Hebert <hebertdev82@gmail.com>
2024-05-14 14:14:28 -0300

    feat: including citation section

--- app/components/citation.jsx
+++ app/components/citation.jsx
@@ -12,7 +12,7 @@ export default function Citation() {
       <div className="text-center">
         <a
           href="#"
-          className="px-8 py-3 text-center bg-[#2B3E59] text-white hover:transition-all hover:bg-[#A5B9D3] hover:text-[#211C1B] rounded-full mx-3 md:text-4xl text-2xl font-bold"
+          className="px-6 py-3 text-center bg-[#2B3E59] text-white hover:transition-all hover:bg-[#A5B9D3] hover:text-[#211C1B] rounded-full mx-3 md:text-4xl text-2xl font-bold"
         >
           Faça uma cotação conosco
         </a>
