0ee126014440f92ff91056d0eefd3b78b9c4c896
Hebert <hebertdev82@gmail.com>
2024-05-31 14:52:48 -0300

    feat: construction page

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,18 @@ import HomePage from "./pages/home/page";
 export default function Home() {
   return (
     <>
-      <HomePage />
+      {/* <HomePage /> */}
+      <div className="flex h-screen text-center">
+        <div className="m-auto">
+          <h2 className="text-4xl font-bold text-[#2B3E59]">
+            Site em Construção
+          </h2>
+          <h4 className="text-xl">
+            Estamos preparando um lindo site! Em breve, disponibilizaremos para
+            você!
+          </h4>
+        </div>
+      </div>
     </>
   );
 }
