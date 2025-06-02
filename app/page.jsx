aa5da11cd27375ca86cbc20d6e119d5c605071dd
Hebert <hebertdev82@gmail.com>
2025-06-02 15:21:36 -0300

    feat: cleaning home page

--- app/page.jsx
+++ app/page.jsx
@@ -4,17 +4,6 @@ export default function Home() {
   return (
     <>
       <HomePage />
-      {/* <div className="flex h-screen text-center">
-        <div className="m-auto">
-          <h2 className="text-4xl font-bold text-[#2B3E59]">
-            Site em Construção
-          </h2>
-          <h4 className="text-xl">
-            Estamos preparando um lindo site! Em breve, disponibilizaremos para
-            você!
-          </h4>
-        </div>
-      </div> */}
     </>
   );
 }
